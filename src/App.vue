b70825a614743abb407b691b978019ffe30cd694
daeun <sung980524@naver.com>
2021-07-25 19:22:13 +0900

    데이터 넣어서 목록 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="black-bg" v-if="isModalOpen">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="isModalOpen=false">닫기</button>
+    </div>
+  </div>
+  <div class="menu">
+    <a v-for="(menu,index) in menus" :key="index">
+      {{menu}}
+    </a>
+  </div>
+  <div>
+    <div v-for="data in onerooms" :key="data.id">
+      <img :src="data.image" class="room-img">
+      <h4>{{data.title}}</h4>
+      <p>{{data.price}}원</p>
+      <p>{{data.content}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './oneroom';
 
 export default {
   name: 'App',
+  data(){
+    return {
+      onerooms : data,
+      isModalOpen : false,
+      신고수 : [0,0,0],
+      menus:['Home','Shop','About'],
+      products: data,
+    }
+  },
+  methods:{
+    increase(num){
+      this.신고수[num]+=1;
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+} 
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+.room-img{
+  width: 100%;
+  margin-top: 40px;
+}
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a{
+  color: white;
+  padding: 10px;
+  font-weight: 700;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
